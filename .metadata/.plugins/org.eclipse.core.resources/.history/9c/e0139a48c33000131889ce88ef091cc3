e12c1f0ba9d848f732fe77944b86e0fb55a55d7a
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-09 05:18:32 -0400

    Dog Years problem fixes
    changed variable sparkyDogYears to dogYears and added more elaborative
    comments

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9c/e0139a48c33000131889ce88ef091cc3
@@ -0,0 +1,9 @@
+// Andrew Tillett 10/9/13 Expressions_worksheet Dog Years Problem
+
+
+//Calculate Sparky the pit bull's age in dog years
+
+var sparkyHumanYears = 4;// Setting up the variable for Sparky's age in human years
+var sparkyDogYears = 7;// Setting up the variable for Sparky's age in dog years
+var result = "Sparky is " + sparkyHumanYears + " human years old which is " + sparkyHumanYears * sparkyDogYears + " in dog years.";// Setting up the variable with an expression and strings for the result of Sparky's age in dog years based on how old his is in human years
+console.log(result);// Printing the result variable to the console
--- Expressions_worksheet/Dog_years/js/script.js
+++ Expressions_worksheet/Dog_years/js/script.js
@@ -4,6 +4,6 @@
 //Calculate Sparky the pit bull's age in dog years
 
 var sparkyHumanYears = 4;// Setting up the variable for Sparky's age in human years
-var sparkyDogYears = 7;// Setting up the variable for Sparky's age in dog years
-var result = "Sparky is " + sparkyHumanYears + " human years old which is " + sparkyHumanYears * sparkyDogYears + " in dog years.";// Setting up the variable with an expression and strings for the result of Sparky's age in dog years based on how old his is in human years
+var dogYears = 7;// Setting up the variable for Sparky's age in dog years
+var result = "Sparky is " + sparkyHumanYears + " human years old which is " + sparkyHumanYears * dogYears + " in dog years.";// Setting up the variable with strings and an expression for the result of Sparky's age in dog years based on how old he is in human years by multiplying the sparkyHumanYears and dogYears variables to get the dog years answer
 console.log(result);// Printing the result variable to the console
