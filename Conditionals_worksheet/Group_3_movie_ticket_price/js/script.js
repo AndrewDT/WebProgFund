f86b6770c36a301ff314c1d3e24e68ab4bfddcc2
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-14 00:38:52 -0400

    Group 2 Check the Login Problem Complete

--- Conditionals_worksheet/Group_3_movie_ticket_price/js/script.js
+++ Conditionals_worksheet/Group_3_movie_ticket_price/js/script.js
@@ -1,1 +1,15 @@
-alert("This is in the js file");
+//Andrew Tillett 10/13/13 Conditionals Worksheet Group 3 Movie Ticket Price Problem
+
+/*Criteria:
+
+For each solution you will need the following:
+Label the section of code appropriately
+Write the givens as variables
+The result should be calculated using variables, not literal values when possible.
+Create code that will make decisions based on the value of the givens
+Print the result of the decision-making to the console.
+Use only code and techniques learned in this class.
+
+Problem:
+
+*/
