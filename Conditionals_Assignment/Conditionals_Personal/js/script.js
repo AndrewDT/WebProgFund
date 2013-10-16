ef45406a445ee1ada7ddd2c3a89eff8814bbf069
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-16 12:45:29 -0400

    Conditionals_Personal Finalized ///2
    Part of the last commit.

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -38,13 +38,19 @@ Result: "You are lifting xlbs for x reps, which means this weight is perfect!", 
 
 
 var currentWeight = prompt("Please enter the current weight you are lifting (in lbs)");
+var emptyWeightField;
 if(currentWeight === ""){
 	var emptyWeightField = prompt("NOTE: CURRENT WEIGHT MUST BE ENTERED FOR A CORRECT IDEAL WEIGHT DETERMINATION");
 }
 var reps = prompt("Please enter the number of perfect repetitions you can perform at your current weight load");
+var emptyRepsField;
 if(reps === ""){
 	var emptyRepsField = prompt("NOTE: REPETITIONS AMOUNT MUST BE ENTERED FOR A CORRECT IDEAL WEIGHT DETERMINATION");
 }
+
+tooLight = reps > 20 || emptyRepsField > 20;
+tooHeavy = reps <= 3 || emptyRepsField <= 3 ;
+justRight = (reps >= 4 && reps <= 20) || (emptyRepsField >= 4 && emptyRepsField <= 20);
 
 
 var heavyReduction = .10;
@@ -52,13 +58,14 @@ var lightIncrease = .20;
 
 var perfectWeight;
 
-if(reps <= 3 || emptyRepsField <= 3){
-	var perfectWeight = currentWeight - currentWeight*heavyReduction || emptyWeightField - emptyWeightField*heavyReduction;
-	console.log("Sorry, you can only do " + (reps || emptyRepsField) + " reps at " + (currentWeight || emptyWeightField) + "lbs, so the weight is too heavy for you right now! Try " + perfectWeight + "lbs.");
-}else if(reps > 20 || emptyRepsField > 20){
+
+if(tooLight){
 	var perfectWeight = Number(currentWeight) + Number(currentWeight)*Number(lightIncrease) || Number(emptyWeightField) + Number(emptyWeightField)*Number(lightIncrease);
-	console.log("You can do " + (reps || emptyRepsField) + " reps at " + (currentWeight || emptyWeightField) + "lbs, that's lightweight for you! Looks like you need to go heavier! Try " + perfectWeight + "lbs.");
-}else if((reps >= 4 && reps <= 20) || (emptyRepsField >= 4 && emptyRepsField <= 20)){
+	console.log("You can do " + (reps || emptyRepsField) + " reps at " + (currentWeight || emptyWeightField) + "lbs, that's lightweight for you! Looks like you need to go heavier! Try " + perfectWeight+"lbs");
+}else if(justRight){
 	var perfectWeight = currentWeight || emptyWeightField;
 	console.log("You are lifting " + perfectWeight + "lbs for " + (reps || emptyRepsField) + " reps, which means this weight is just right!");
+}else if(tooHeavy){
+		var perfectWeight = Number(currentWeight) - Number(currentWeight)*Number(lightIncrease) || Number(emptyWeightField) - Number(emptyWeightField)*Number(lightIncrease);
+			console.log("Sorry, you can only do " + (reps || emptyRepsField) + " reps at " + (currentWeight || emptyWeightField) + "lbs, right now. Try " + perfectWeight + "lbs.");
 }
