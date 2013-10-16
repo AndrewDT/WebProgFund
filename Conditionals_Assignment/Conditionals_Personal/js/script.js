df6fe139a479281323784a700d9192cb608c89af
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-15 23:19:28 -0400

    Conditionals_Personal problem, givens, and result statements added

--- Conditionals_Assignment/Conditionals_Personal/js/script.js
+++ Conditionals_Assignment/Conditionals_Personal/js/script.js
@@ -1,1 +1,31 @@
-alert("This is in the js file");
+// Andrew Tillett 10/15/10 Assignment Conditionals Conditionals_Personal
+
+/*Criteria:
+
+Make sure each of the sub-projects follows this list of criteria:
+As in the Expressions assignment you will need to create code that calculates a result. You can not use the same formulas or code used in that previous assignment, however.
+Decide what your code will calculate. This should be an original calculation. In other words you may not use any of the examples mentioned in this class. This will result in an automatic 0.
+Your calculation should use a minimum of two factors or givens.
+At least two factors must be essential for the calculation. If one of the factors (the pieces of information you are collecting with the prompts) is not needed in order to calculate the result then it is not essential.
+There should be a minimum of two prompts to collect user information. The prompts should include labels so the user knows what information they need to enter. These prompts should assign the information collected into variables you have set up.
+Write the givens as appropriately named variables.
+Declare a variable for the resulting value of the calculation.
+Calculate the result using the variables in an expression (NOT the literal values or numbers)
+Assign the calculation to the resulting variable.
+The result should appear in the browser's console. (Remember to include units like “square feet” or "meters cubed"). This should explain the result as well.
+Validation
+In each of the sub-projects you must use conditionals to validate the information the user enters into the prompts.
+For example, if the user does not enter a value for the first prompt, make a conditional that checks if the value is empty. You can do this by evaluating if the contents of the prompt are the same as an empty string (which looks like "" i.e. thats two quotations with no character between them)
+Comment every line of code (describe what each line is doing in English). Do NOT just label sections of your code.
+Each of the following requirements must exist in at least one of the 3 subprojects:
+A ternary
+An else if
+A logical operator
+At least one relational operator other than ==
+Commiting to your repository:
+You are required to make at least 20 commits to your repository for each project as you develop your code.
+Failing to do so will result in a 0 for the assignment.
+
+Problem:
+When lifting, it is important to adjust weight according to the maximum number of repetitions you can do at that weight. If the weight allows you to do 3 or less perfect repetitions, the weight should be decreased by 10%, and if the weight allows you to do more than 20 repetitions, the weight should be increased by 20%.
+/*
