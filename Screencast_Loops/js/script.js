f98636168f2d089f5cdee34f9a83e0c47a077604
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-20 22:06:09 -0400

    Screencast: Loops folder added and walked through

---
+++ Screencast_Loops/js/script.js
@@ -0,0 +1,78 @@
+//Screencast: While Loop
+/*
+While Loop: Looks like an if conditional statement
+
+var b = 10;//Sets up the index
+
+While Loop
+
+while b is greater than 0 than run this condition
+while(b > 0){//Checks the condition
+	console.log(b + "kegs on the wall");
+	b--;//Increments or decrements the index
+}
+*/
+
+
+//Screencast: Do While Loop
+/*
+While Loop: Looks like an if conditional statement
+
+var b = 10;//Sets up the index
+
+While Loop
+
+while b is greater than 0 than run this condition
+while(b > 0){//Checks the condition
+	console.log(b + "kegs on the wall");
+	b--;//Increments or decrements the index
+} 
+
+var c = 10;
+
+do{
+	console.log(c + "kegs on the wall");
+	c--;
+}
+while(c > 0);
+//Gives us the same result as the first while loop
+//While must come after the do
+//DO run this code WHILE the condition is true
+//Code still runs even if false though
+*/
+
+
+//For Loop
+/*While Loop: Looks like an if conditional statement
+
+var b = 10;//Sets up the index
+
+While Loop
+
+while b is greater than 0 than run this condition
+while(b > 0){//Checks the condition
+	console.log(b + "kegs on the wall");
+	b--;//Increments or decrements the index
+} 
+
+var c = 10;
+
+do{
+	console.log(c + "kegs on the wall");
+	c--;
+}
+while(c > 0);
+//Gives us the same result as the first while loop
+//While must come after the do
+//DO run this code WHILE the condition is true
+//Code still runs even if false though
+
+
+//For loop is a combination of all loops put together. Also known as the "Short loop". Does everything in one like (Like a ternary maybe?)
+
+for(var i = 10; i > 0; i--){
+	console.log(i + "kegs on the wall");
+}
+
+*/
+
