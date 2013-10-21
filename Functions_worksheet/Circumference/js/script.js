82aae0fe0d54e7e7029e1656413de8cc36e8af56
Andrew Tillett <AndrewD.Tillett@gmail.com>
2013-10-21 14:52:55 -0400

    Circumference Problem Additions
    Added Criteria, Example, and Problem information to comments

--- Functions_worksheet/Circumference/js/script.js
+++ Functions_worksheet/Circumference/js/script.js
@@ -1,3 +1,37 @@
+//Andrew Tillett 10/21/13 Functions Worksheet Circumference Problem
 
+/*Criteria:
+For each solution you will need the following:
+Comment each line of code appropriately. Comment, comment, comment!
+Write the givens as variables, and pass the givens to the function using arguments.
+The calcultions should be within the functions and the function should return the result to the result variable.
+Create a variable for your result (don’t forget!)
+Print the result using the result variable and the appropriate message outside the function. (Do not print from the functions you create)
+There should be a minimum of 20 commits to your git repository.
+This should all be done in a single project and single .as file.
+
+
+Example:
+Calculate the area of a rectangle given the width and height of the rectangle.
+//Calculate the Area of a Rectangle
+var width = 5;
+var height = 6;
+var area = calculateArea(width, height);
+console.log(“The Area of the Rectangle is “ + area);
+private function calculateArea(w, h ):Number
+{
+     return w *h;
+}
+
+Problem: Circumference
+
+Calculate the circumference of a circle.
+Parameter(s) for function:
+Radius of the circle
+Return:
+Circumference of the circle
+Result to print to the console:
+“The circumference of the circle is X”;
+*/
 
 
